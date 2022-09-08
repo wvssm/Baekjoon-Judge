d3cfffae8da4a7a1267a1c7321ade95b769a1482
wvssm <52875244+wvssm@users.noreply.github.com>
2022-09-09 03:55:42 +0900

    [백준 15552] 빠른 A+B

---
+++ 0x02기초코드작성요령/15552.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/15552
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t, a, b;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+}
