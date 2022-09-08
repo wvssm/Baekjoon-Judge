0004eecdf14c0bf46fa29ac84f2c1addbc6718d1
wvssm <52875244+wvssm@users.noreply.github.com>
2022-09-09 03:56:50 +0900

    [백준 2440] 별 찍기 - 3

---
+++ 0x02기초코드작성요령/2440.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < i; j++) cout << "*";
+		cout << "\n";
+	}
+
+	/*for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - i; j++) cout << "*";
+		cout << "\n";
+	}*/
+}
