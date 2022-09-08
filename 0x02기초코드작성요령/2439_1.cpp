de93b8c59ba042f082da723dda991bc36d546b9d
wvssm <52875244+wvssm@users.noreply.github.com>
2022-09-09 03:56:36 +0900

    [백준 2439] 별 찍기 - 2

---
+++ 0x02기초코드작성요령/2439_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	string s;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int j = 0;
+		for (; n - i - 1; j++) cout << ' ';
+		for (; j < n; j++) cout << "*";
+		cout << "\n";
+	}
+}
+
+// 공백이랑 *이어진다고 생각하고 for문 작성 
