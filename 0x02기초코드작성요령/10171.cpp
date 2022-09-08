42d762088b9a95d6216a3661f7964824ff67c85e
wvssm <52875244+wvssm@users.noreply.github.com>
2022-09-09 03:49:56 +0900

    백준 [10171] 고양이

---
+++ 0x02기초코드작성요령/10171.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout << "\\    /\\" << "\n";
+	cout << " )  ( \')" << "\n";
+	cout << "(  /  )" << "\n";
+	cout << " \\(__)|" << "\n";
+}
+
+// 특수문자 입출력을 할 줄 아느냐 묻는 문제
